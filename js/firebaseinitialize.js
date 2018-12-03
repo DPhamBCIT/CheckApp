698c4801d1e11a460afaeaff3be50bb0a6bfd553
BrendonHorning <43552900+BrendonHorning@users.noreply.github.com>
2018-12-03 11:06:41 -0800

    Added js folder + scripts, modified review.html

---
+++ js/firebaseinitialize.js
@@ -0,0 +1,9 @@
+var config = {
+		apiKey: "AIzaSyBvI9C9xDjGb5c8exGw5OiNduMWQFf-0dY",
+		authDomain: "checkapp-data.firebaseapp.com",
+		databaseURL: "https://checkapp-data.firebaseio.com",
+		projectId: "checkapp-data",
+		storageBucket: "checkapp-data.appspot.com",
+		messagingSenderId: "385146664281"
+	};
+  firebase.initializeApp(config);
